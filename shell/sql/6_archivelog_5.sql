0ff38b3fd9ebc2eae1edcbed0202814ef75d8a01
bsshin71 <beumsu@gmail.com>
2018-05-24 10:39:21 +0900

    add new

---
+++ shell/sql/6_archivelog_5.sql
@@ -0,0 +1,10 @@
+set feedback off;
+set timing off;
+set pagesize 30
+SET LINESIZE 1024;
+SET COLSIZE 60;
+select
+       'The current version does not support this view yet(시간대별로 생성된 archivelog 갯수 표시 )!!!' as Message
+from dual 
+--sqlend
+;
